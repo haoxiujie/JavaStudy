2746e42c66934492567b988e2e464d4266d4b84b
haoxiujie <haoxiujie@gmail.com>
2017-11-04 12:35:53 +0800

    20171104
    Signed-off-by: haoxiujie <haoxiujie@gmail.com>

---
+++ src/hello/Score.java
@@ -0,0 +1,20 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Score {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		double sum = 0.0;
+		int sign = 1;
+		for ( int i = 1; i<=n; i++, sign = -sign)
+		{
+			sum += sign*1.0/i;			
+		}
+		System.out.printf("%.2f",sum);
+		in.close();
+	}
+
+}
