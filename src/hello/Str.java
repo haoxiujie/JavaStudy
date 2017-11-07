234fb22be85b4b6ff79bcf183f091bad0f217545
haoxiujie <haoxiujie@gmail.com>
2017-11-07 11:32:54 +0800

    20171107
    Signed-off-by: haoxiujie <haoxiujie@gmail.com>

---
+++ src/hello/Str.java
@@ -0,0 +1,15 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Str {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine();
+		System.out.println(s);
+		in.close();
+
+	}
+
+}
