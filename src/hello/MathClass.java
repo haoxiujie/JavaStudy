39a58ec0b7cc73286c28864ce0205386a1714634
haoxiujie <haoxiujie@gmail.com>
2017-11-07 12:36:48 +0800

    20171107
    Signed-off-by: haoxiujie <haoxiujie@gmail.com>

---
+++ src/hello/MathClass.java
@@ -0,0 +1,22 @@
+package hello;
+
+import java.util.Scanner;
+
+public class MathClass {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		
+		//Math类
+		//abs 绝对值
+		//pow 幂次
+		//random [0,1)随机数
+		//round 四舍五入
+		
+		System.out.println(Math.abs(-12));
+		System.out.println(Math.round(10.645));
+		System.out.println(Math.random()*100);
+		System.out.println(Math.pow(2,3.2));
+	}
+
+}
--- src/hello/Str.java
+++ src/hello/Str.java
@@ -6,10 +6,23 @@ public class Str {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		String s = in.nextLine();
-		System.out.println(s);
+//		String s = in.nextLine();
+//		System.out.println(s);
+//		System.out.println(s.equals("bye"));
+		
+//		String s1 = "abc";
+//		String s2 = "abcd";
+//		System.out.println(s1.compareTo(s2));
+		
+		String name = "0123456789ºº×Ö";
+//		System.out.println(name.length());
+		System.out.println(name.substring(2,4));
+//		for( int i=0; i<name.length();i++)
+//		{
+//			System.out.println(name.charAt(i));
+//		}
+		
 		in.close();
-
 	}
 
 }
