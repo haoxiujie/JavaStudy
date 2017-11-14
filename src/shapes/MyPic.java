f18dd039043f89e07137de99a7177b905158fc23
haoxiujie <haoxiujie@gmail.com>
2017-11-14 10:56:14 +0800

    20171114
    Signed-off-by: haoxiujie <haoxiujie@gmail.com>

--- src/shapes/MyPic.java
+++ src/shapes/MyPic.java
@@ -4,16 +4,18 @@ public class MyPic {
 	public static void main(String[] args) 
 	{
 		Picture pic = new Picture(420,300);
-		Circle c1 = new Circle(320,40,80);
+		Circle c1 = new Circle(320,40,40);
 		Rectangle r1 = new Rectangle(100, 100, 100, 100);
 		Triangle t1 = new Triangle(100, 100, 200, 100, 150, 50);
 		Line l1 = new Line(0,205,400,205);
 		Circle c2 = new Circle(200,200,50);
+		Line l2 = new Line(3,209,350,250);
 		pic.add(c1);
 		pic.add(r1);
 		pic.add(t1);
 		pic.add(l1);
 		pic.add(c2);
+		pic.add(l2);
 		pic.draw();	
 	}
 }
