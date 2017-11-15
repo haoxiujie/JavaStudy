0a4cbb38a623ac55a83d2e8a715b59846dd54783
haoxiujie <haoxiujie@gmail.com>
2017-11-15 20:33:30 +0800

    20171115
    Signed-off-by: haoxiujie <haoxiujie@gmail.com>

--- src/oop_digitalClock/Display.java
+++ src/oop_digitalClock/Display.java
@@ -1,6 +1,10 @@
 package oop_digitalClock;
 
+//	一个类是pubic的，表明任何人都可以用这个类的定义来定义变量。即new出这个类来。
+//	但是public类有个要求就是，文件名必须和这个类名相同。
+//	一个文件(编译单元)可以有很多类，但是只有一个类能是public的。
 public class Display {
+	
 //	private 私有的 用于成员变量和成员函数前面
 //	说明这个成员是这个类私有的，只有在这个类内部才能访问它。(即这个类的大括号里面。)
 	private int value = 0;//（当前）值
