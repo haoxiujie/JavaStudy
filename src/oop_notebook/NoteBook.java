29520521ae588ef9e776a07e9eea7f8e7d7afc79
haoxiujie <haoxiujie@gmail.com>
2017-11-16 17:56:37 +0800

    20171116
    Signed-off-by: haoxiujie <haoxiujie@gmail.com>

---
+++ src/oop_notebook/NoteBook.java
@@ -0,0 +1,33 @@
+package oop_notebook;
+
+public class NoteBook {
+	
+	public void add(String s) {
+		//添加笔记
+	}
+	
+	public int getSize() {
+		//查询数量 
+		return 0;
+	}
+	
+	public String getNote(int index) {
+		//查询笔记
+		return "";
+	}
+	
+	public boolean removeNote(int index) {
+		//删除笔记
+		return true;
+	}
+	
+	public String[] list() {
+		return null;		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
