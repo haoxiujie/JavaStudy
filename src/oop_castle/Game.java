4343b6b71f3e88ee1d56f486460e044cb562be42
haoxiujie <haoxiujie@gmail.com>
2017-12-05 21:06:47 +0800

    20171205
    Signed-off-by: haoxiujie <haoxiujie@gmail.com>

--- src/oop_castle/Game.java
+++ src/oop_castle/Game.java
@@ -1,19 +1,24 @@
 package oop_castle;
-
+//设计原则(目标)(要考虑的东西)：
+//1.消除代码复制
+//2.封装
+//3.可扩展性
+//4.框架加数据
+//5.类型系统
 import java.util.Scanner;
 
 public class Game {
-    private Room currentRoom;
+    private Room currentRoom;//当前房间
         
     public Game() 
     {
-        createRooms();
+        createRooms();//进入房间函数
     }
 
     private void createRooms()
     {
         Room outside, lobby, pub, study, bedroom;
-      
+      //当前的房间有城堡外，大堂，小酒吧，书房，卧室
         //	制造房间
         outside = new Room("城堡外");
         lobby = new Room("大堂");
@@ -27,7 +32,7 @@ public class Game {
         pub.setExits(null, outside, null, null);
         study.setExits(outside, bedroom, null, null);
         bedroom.setExits(null, null, null, study);
-
+        //描述各个房间的出口信息，分别是Room类的北，东，南，西方向
         currentRoom = outside;  //	从城堡门外开始
     }
 
@@ -37,17 +42,7 @@ public class Game {
         System.out.println("这是一个超级无聊的游戏。");
         System.out.println("如果需要帮助，请输入 'help' 。");
         System.out.println();
-        System.out.println("现在你在" + currentRoom);
-        System.out.print("出口有：");
-        if(currentRoom.northExit != null)
-            System.out.print("north ");
-        if(currentRoom.eastExit != null)
-            System.out.print("east ");
-        if(currentRoom.southExit != null)
-            System.out.print("south ");
-        if(currentRoom.westExit != null)
-            System.out.print("west ");
-        System.out.println();
+        showPrompt();//->消除了代码复制
     }
 
     // 以下为用户命令
@@ -59,8 +54,9 @@ public class Game {
     }
 
     private void goRoom(String direction) 
-    {
-        Room nextRoom = null;
+    {//函数：去房间，传来的值是方向
+        Room nextRoom = null;//先将准备进入的房间名初始化
+        //
         if(direction.equals("north")) {
             nextRoom = currentRoom.northExit;
         }
@@ -73,34 +69,43 @@ public class Game {
         if(direction.equals("west")) {
             nextRoom = currentRoom.westExit;
         }
+        //如果输入的内容和方向一致，则进入房间或被赋值null
 
         if (nextRoom == null) {
             System.out.println("那里没有门！");
         }
         else {
             currentRoom = nextRoom;
-            System.out.println("你在" + currentRoom);
-            System.out.print("出口有: ");
-            if(currentRoom.northExit != null)
-                System.out.print("north ");
-            if(currentRoom.eastExit != null)
-                System.out.print("east ");
-            if(currentRoom.southExit != null)
-                System.out.print("south ");
-            if(currentRoom.westExit != null)
-                System.out.print("west ");
-            System.out.println();
+            showPrompt();//->消除了代码复制
+            //如果有则进入，输出当前房间的四周状态-，此处存在代码复制-
         }
     }
 	
+    public void showPrompt() {
+    	System.out.println("你在" + currentRoom);
+        System.out.print("出口有: ");
+        if(currentRoom.northExit != null)
+            System.out.print("north ");
+        if(currentRoom.eastExit != null)
+            System.out.print("east ");
+        if(currentRoom.southExit != null)
+            System.out.print("south ");
+        if(currentRoom.westExit != null)
+            System.out.print("west ");
+        //如果输入的方向有房间，则输出方向
+        System.out.println();
+    }
+    
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		Game game = new Game();
 		game.printWelcome();
-
+		//开始游戏
         while ( true ) {
         		String line = in.nextLine();
+        		//获取一整行字符
         		String[] words = line.split(" ");
+//↑String.split->将一个字符串按“”中的内容分割为子字符串，然后将结果作为字符串数组返回。
         		if ( words[0].equals("help") ) {
         			game.printHelp();
         		} else if (words[0].equals("go") ) {
--- src/oop_castle/Room.java
+++ src/oop_castle/Room.java
@@ -1,31 +1,31 @@
 package oop_castle;
 
 public class Room {
-    public String description;
-    public Room northExit;
-    public Room southExit;
-    public Room eastExit;
-    public Room westExit;
+    public String description;//当前房间描述
+    public Room northExit;//北边出口
+    public Room southExit;//南边出口
+    public Room eastExit;//东边出口
+    public Room westExit;//西边出口
 
     public Room(String description) 
     {
-        this.description = description;
+        this.description = description;//构造函数，当前房间
     }
 
     public void setExits(Room north, Room east, Room south, Room west) 
-    {
-        if(north != null)
+    {//↑获取出口信息
+        if(north != null)//如果北边不是null，则有北边出口，并赋值北边房间名字
             northExit = north;
-        if(east != null)
+        if(east != null)//如果东边不是null，则有东边出口，并赋值东边房间名字
             eastExit = east;
-        if(south != null)
+        if(south != null)//如果南边不是null，则有南边出口，并赋值南边房间名字
             southExit = south;
-        if(west != null)
+        if(west != null)//如果西边不是null，则有西边出口，并赋值西边房间名字
             westExit = west;
     }
 
     @Override
-    public String toString()
+    public String toString()//重写toString，返回当前房间描述
     {
         return description;
     }
